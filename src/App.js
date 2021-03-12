2e6240ef6d8a22124e0c4c1e67ce120e751a9578
iulian-coder <iulian.titei@gmail.com>
2021-03-12 11:01:48 +0200

    add .eslintcache

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const AuthenticatedApp = lazy(() => import("./authenticated-app"));
 const UnauthenticatedApp = lazy(() => import("./unauthenticated-app"));
 // Suspense lets components “wait” for something before rendering. 
 // Suspense only supports one use case: loading components dynamically with React.lazy.
-// https://reactjs.org/docs/react-api.html
+
 function App() {
   const { user } = useAuthState();
   return (
